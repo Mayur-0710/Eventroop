bbed401263c90053197d77da42cb855bcece98dd
Pankaj Shinde <pankaj.shinde.codes@gmail.com>
2026-03-21 14:59:46 +0530

    update asgi config

--- eventroop_backend/eventroop_backend/asgi.py
+++ eventroop_backend/eventroop_backend/asgi.py
@@ -1,11 +1,21 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventroop_backend.settings')
+
 from django.core.asgi import get_asgi_application
+
+# Step 1: initialize Django FIRST
+django_asgi_app = get_asgi_application()
+
+# Step 2: import AFTER Django is ready
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from notification.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventroop_backend.settings')
+# Step 3: define application
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
 })

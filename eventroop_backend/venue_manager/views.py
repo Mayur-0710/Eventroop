a3e5439689d38be24df8d1e9af1facbc6359a470
Pankaj Shinde <pankaj.shinde.codes@gmail.com>
2026-04-14 12:06:48 +0530

    Developed private service dropdown

--- eventroop_backend/venue_manager/views.py
+++ eventroop_backend/venue_manager/views.py
@@ -89,6 +89,7 @@ class ServiceViewSet(viewsets.ModelViewSet):
 
     filterset_fields = {
         "is_active": ["exact"],
+        "service_type": ["iexact"],
         "venue": ["exact"],
         "venue__location": ["exact"],
         "manager": ["exact"],
